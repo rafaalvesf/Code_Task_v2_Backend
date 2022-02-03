2267178688c539461efec3ae5b267ba2fe2d0c2e
Rafael <rafael.alves.1@hotmail.com>
2022-02-02 21:15:20 -0300

    Subindo projeto

---
+++ src/main/java/com/codetask/codeTask/CodeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.codetask.codeTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetask/codeTask/CodeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetask.codeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
