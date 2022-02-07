dc32aabd3d9a3dab6903dcfe74eeaffca8be4992
Rafael <rafael.alves.1@hotmail.com>
2022-02-07 11:35:19 -0300

    Configurando retorno de veiculos por ponto

--- src/main/java/com/codetask/codeTask/service/GetTempoService.java
+++ src/main/java/com/codetask/codeTask/service/GetTempoService.java
@@ -1,7 +1,5 @@
 package com.codetask.codeTask.service;
 
-import com.codetask.codeTask.controller.CadastraPOIController;
-import com.codetask.codeTask.controller.CadastraPosicoesController;
 import com.codetask.codeTask.model.CadastraPOIModel;
 import com.codetask.codeTask.model.CadastraPosicoesModel;
 import com.codetask.codeTask.model.GetTempoModel;
@@ -14,31 +12,10 @@ import java.util.List;
 @Service
 public class GetTempoService {
     @Autowired
-    CadastraPOIController cadastraPOIController;
-    CadastraPosicoesController cadastraPosicoesController;
+
 
     ArrayList<CadastraPOIModel> listaPOi = new ArrayList();
     ArrayList<CadastraPosicoesModel> listaPosicoes = new ArrayList();
 
 
-    public List<GetTempoModel> getTempo() {
-        ArrayList<GetTempoModel> listaTempo = new ArrayList();
-
-        String teste = retornaNome();
-
-        listaTempo.add(new GetTempoModel(teste, "teste", "teste"));
-        listaTempo.add(new GetTempoModel("teste", "teste", "teste"));
-        listaTempo.add(new GetTempoModel("teste", "teste", "teste"));
-
-
-        return listaTempo;
-    }
-    public String retornaNome(){
-        listaPOi.addAll(cadastraPOIController.findAll());
-        listaPosicoes.addAll(cadastraPosicoesController.findAll());
-
-        String teste = listaPOi.get(1).getNome();
-
-    return teste;
-    }
 }
